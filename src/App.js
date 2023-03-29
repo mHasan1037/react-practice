9a1ed20454004ef5dfa80875a8b7c26da868eef1
Mahmudul Hasan <mhasan.bd.95@gmail.com>
2023-03-29 22:57:49 +0600

    practicing redux react

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from 'react'
-import Create from './Components/Create'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Read from './Components/Read'
-import Update from './Components/Update'
+import Counter from './component/Counter'
+
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-         <Route exact path='/' element={<Create />} />
-         <Route exact path="/read" element={<Read />} />
-         <Route exact path='/update' element={<Update />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Counter />
+    </>
   )
 }
 
